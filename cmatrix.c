0bf641187df85064832becbbf9676c6b61f7c97c
a1346054 <36859588+a1346054@users.noreply.github.com>
2021-08-27 22:58:21 +0000

    Unify codestyle
    No changes in functionality.

--- cmatrix.c
+++ cmatrix.c
@@ -245,7 +245,7 @@ void resize_screen(void) {
     CONSOLE_SCREEN_BUFFER_INFO csbiInfo;
 
     hStdHandle = GetStdHandle(STD_OUTPUT_HANDLE);
-    if(hStdHandle == INVALID_HANDLE_VALUE)
+    if (hStdHandle == INVALID_HANDLE_VALUE)
         return;
 #else
     char *tty;
@@ -259,7 +259,7 @@ void resize_screen(void) {
 #endif
 #ifdef _WIN32
     result = GetConsoleScreenBufferInfo(hStdHandle, &csbiInfo);
-    if(!result)
+    if (!result)
         return;
     LINES = csbiInfo.dwSize.Y;
     COLS = csbiInfo.dwSize.X;
@@ -278,10 +278,10 @@ void resize_screen(void) {
     LINES = win.ws_row;
 #endif
 
-    if(LINES <10){
+    if (LINES < 10) {
         LINES = 10;
     }
-    if(COLS <10){
+    if (COLS < 10) {
         COLS = 10;
     }
 
@@ -493,7 +493,7 @@ if (console) {
             init_pair(COLOR_YELLOW, COLOR_YELLOW, -1);
         } else {
 #else
-        { /* Hack to deal the after effects of else in HAVE_USE_DEFAULT_COLOURS*/
+        { /* Hack to deal the after effects of else in HAVE_USE_DEFAULT_COLOURS */
 #endif
             init_pair(COLOR_BLACK, COLOR_BLACK, COLOR_BLACK);
             init_pair(COLOR_GREEN, COLOR_GREEN, COLOR_BLACK);
@@ -507,7 +507,7 @@ if (console) {
     }
 
     /* Set up values for random number generation */
-    if(classic) {
+    if (classic) {
         /* Japanese character unicode range [they are seen in the original cmatrix] */
         randmin = 12288;
         highnum = 12351;
@@ -526,7 +526,7 @@ if (console) {
 #ifndef _WIN32
         /* Check for signals */
         if (signal_status == SIGINT || signal_status == SIGQUIT) {
-            if(lock != 1)
+            if (lock != 1)
                 finish();
             /* exits */
         }
@@ -535,8 +535,8 @@ if (console) {
             signal_status = 0;
         }
 
-        if(signal_status == SIGTSTP){
-            if(lock != 1)
+        if (signal_status == SIGTSTP) {
+            if (lock != 1)
                     finish();
         }
 #endif
@@ -567,7 +567,7 @@ if (console) {
                 case 3: /* Ctrl-C. Fall through */
 #endif
                 case 'q':
-                    if(lock != 1)
+                    if (lock != 1)
                         finish();
                     break;
                 case 'a':
@@ -642,7 +642,7 @@ if (console) {
         for (j = 0; j <= COLS - 1; j += 2) {
             if ((count > updates[j] || asynch == 0) && pause == 0) {
 
-                /* I dont like old-style scrolling, yuck */
+                /* I don't like old-style scrolling, yuck */
                 if (oldstyle) {
                     for (i = LINES - 1; i >= 1; i--) {
                         matrix[i][j].val = matrix[i - 1][j].val;
@@ -706,8 +706,8 @@ if (console) {
                         while (i <= LINES && (matrix[i][j].val != ' ' &&
                                matrix[i][j].val != -1)) {
                             matrix[i][j].is_head = false;
-                            if(changes) {
-                                if(rand() % 8 == 0)
+                            if (changes) {
+                                if (rand() % 8 == 0)
                                     matrix[i][j].val = (int) rand() % randnum + randmin;
                             }
                             i++;
@@ -775,10 +775,10 @@ if (console) {
                         attroff(A_ALTCHARSET);
                     }
                 } else {
-                    if(rainbow) {
+                    if (rainbow) {
                         int randomColor = rand() % 6;
 
-                        switch(randomColor){
+                        switch (randomColor) {
                             case 0:
                                 mcolor = COLOR_GREEN;
                                 break;
@@ -837,7 +837,7 @@ if (console) {
         }
 
         //check if -M and/or -L was used
-        if (msg[0] != '\0'){
+        if (msg[0] != '\0') {
             //Add our message to the screen
             int msg_x = LINES/2;
             int msg_y = COLS/2 - strlen(msg)/2;
@@ -845,7 +845,7 @@ if (console) {
 
             //Add space before message
             move(msg_x-1, msg_y-2);
-            for(i = 0; i < strlen(msg)+4; i++)
+            for (i = 0; i < strlen(msg)+4; i++)
                 addch(' ');
 
             //Write message
@@ -858,7 +858,7 @@ if (console) {
 
             //Add space after message
             move(msg_x+1, msg_y-2);
-            for(i = 0; i < strlen(msg)+4; i++)
+            for (i = 0; i < strlen(msg)+4; i++)
                 addch(' ');
         }
 
@@ -866,4 +866,3 @@ if (console) {
     }
     finish();
 }
-
